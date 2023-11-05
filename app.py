2b4bc052d98093ddf2ed6640b3aabdbf7a1ec80a
Kelsey <Kelseys2071@gmail.com>
2023-11-05 15:26:57 -0600

    updatedapp.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import datetime as dt
+from sqlalchemy import func
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
 #################################################
 # Database Setup
@@ -8,18 +12,23 @@
 
 
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
 # reflect the tables
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Save references to each table
 
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
+app = Flask(__name__)
 #################################################
 
 
